196f2e98b883fddae63e8af46d37280029445734
Lina Yara Brihoum <brihoumlina@gmail.com>
2020-02-29 14:52:31 -0500

    Adding location permissions to android and ios

--- weather_notification/lib/screens/landing_screen.dart
+++ weather_notification/lib/screens/landing_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
-import 'package:weather_notification/exports.dart';
 
 class LandingScreen extends StatelessWidget {
   final String title;
@@ -9,48 +8,53 @@ class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     // api.openweathermap.org/data/2.5/weather?q={city name},{state},{country code}&appid={your api key}
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            WaveWidget(
-              config: CustomConfig(
-                gradients: [
-                  [
-                    Color.fromRGBO(72, 74, 126, 1),
-                    Color.fromRGBO(125, 170, 206, 1),
-                    Color.fromRGBO(184, 189, 245, 0.7)
-                  ],
-                  [
-                    Color.fromRGBO(72, 74, 126, 1),
-                    Color.fromRGBO(125, 170, 206, 1),
-                    Color.fromRGBO(172, 182, 219, 0.7)
-                  ],
-                  [
-                    Color.fromRGBO(72, 73, 126, 1),
-                    Color.fromRGBO(125, 170, 206, 1),
-                    Color.fromRGBO(190, 238, 246, 0.7)
-                  ],
+      body: Stack(
+        children: [
+          WaveWidget(
+            config: CustomConfig(
+              gradients: [
+                [
+                  Color.fromRGBO(72, 74, 126, 1),
+                  Color.fromRGBO(125, 170, 206, 1),
+                  Color.fromRGBO(184, 189, 245, 0.7)
                 ],
-                durations: [19440, 10800, 6000],
-                heightPercentages: [0.03, 0.01, 0.02],
-                gradientBegin: Alignment.bottomCenter,
-                gradientEnd: Alignment.topCenter,
-              ),
-              size: Size(double.infinity, double.infinity),
-              waveAmplitude: 25,
-              backgroundColor: Colors.grey,
+                [
+                  Color.fromRGBO(72, 74, 126, 1),
+                  Color.fromRGBO(125, 170, 206, 1),
+                  Color.fromRGBO(172, 182, 219, 0.7)
+                ],
+                [
+                  Color.fromRGBO(72, 73, 126, 1),
+                  Color.fromRGBO(125, 170, 206, 1),
+                  Color.fromRGBO(190, 238, 246, 0.7)
+                ],
+              ],
+              durations: [19440, 10800, 6000],
+              heightPercentages: [0.03, 0.01, 0.02],
+              gradientBegin: Alignment.bottomCenter,
+              gradientEnd: Alignment.topCenter,
             ),
-            Center(
-              child: Text(
-                'hey',
+            size: Size(double.infinity, double.infinity),
+            waveAmplitude: 25,
+            backgroundColor: Colors.grey,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome',
                 style: TextStyle(
                   color: Colors.white,
+                  fontSize: 20.0,
                 ),
-              )
-            )
-          ],
-        ),
+              ),
+
+            ],
+          ),
+        ],
       ),
     );
   }
